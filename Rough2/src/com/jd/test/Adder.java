d6aa9adb25dc58ee2848b5340581eb7c89efdd1a
Vannala GOUTHAM <vannala.goutham@amadeus.com>
2017-12-26 16:26:21 +0530

    test

---
+++ Rough2/src/com/jd/test/Adder.java
@@ -0,0 +1,23 @@
+/*
+ * Project: TODO: add description
+ * Copyright: 2017-2017 Amadeus
+ *
+ * $Author:$
+ * $Date:$
+ * $Revision:$
+ * $Source:$
+ */
+package com.jd.test;
+
+/**
+ * TODO: add description
+ *
+ * @author vgoutham
+ */
+public class Adder {
+
+    public int add(int value1, int value2) {
+        return value1 + value2;
+    }
+
+}
